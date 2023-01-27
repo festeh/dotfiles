7017e826b16d350a3f6d87e09551e0e47337d3cf
Dima Lipin <dimich3d@ya.ru>
2023-01-27 18:19:57 +0100

    fix script

--- install.sh
+++ install.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-mkdir -p "$XDG_CONFIG_HOME/nvim"
-mkdir -p "$XDG_CONFIG_HOME/nvim/lua/"
-rm -rf "$XDG_CONFIG_HOME/nvim/lua/plugins"
-
-ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim/init.vim"
-ln -sf "$DOTFILES/nvim/lua/options.lua" "$XDG_CONFIG_HOME/nvim/lua/options.lua"
-ln -sf "$DOTFILES/nvim/lua/utils.lua" "$XDG_CONFIG_HOME/nvim/lua/utils.lua"
-ln -sf "$DOTFILES/nvim/lua/keymap.lua" "$XDG_CONFIG_HOME/nvim/lua/keymap.lua"
-ln -sf "$DOTFILES/nvim/lua/plugins" "$XDG_CONFIG_HOME/nvim/lua"
+rm -rf "$XDG_CONFIG_HOME/nvim"
+ln -sf "$DOTFILES/nvim" "$XDG_CONFIG_HOME/nvim"
 
 rm -rf "$XDG_CONFIG_HOME/X11"
 ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME/X11"
@@ -28,16 +21,6 @@ ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
 rm -rf "$XDG_CONFIG_HOME/zsh/external"
 ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
 
-# Install plugin manager for nvim
-[ ! -f "$DOTFILES/nvim/autoload/plug.vim" ] \
-    &&  curl -fLo "$DOTFILES/nvim/autoload/plug.vim" --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-mkdir -p "$XDG_CONFIG_HOME/nvim/autoload"
-ln -sf "$DOTFILES/nvim/autoload/plug.vim" "$XDG_CONFIG_HOME/nvim/autoload/plug.vim"
-
-# Install (or update) nvim plugins
-nvim --noplugin +PlugUpdate +qa
 
 # Install clipboard manager
 [ ! -d "$XDG_CONFIG_HOME/zsh/plugins/zsh-system-clipboard" ] \
