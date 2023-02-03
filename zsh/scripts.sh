6fda3357b097fb9a905d32a337fa5e65b3d71551
Dima Lipin <dimich3d@ya.ru>
2023-02-03 23:23:10 +0100

    auto activate poetry

--- zsh/scripts.sh
+++ zsh/scripts.sh
@@ -7,3 +7,17 @@ take () {
 viman () { 
     text=$(man "$@") && echo "$text" | vim -R +":set ft=man" - ; 
 }
+
+_poetry_exists() {
+    command -v "poetry --version" >/dev/null 2>&1 || return 0
+}
+
+function chpwd() {
+    if [[ -f "pyproject.toml" ]] && _poetry_exists; then
+        poetry shell
+    else
+        if [[ $POETRY_ACTIVE == "1" ]]; then
+            exit
+        fi
+    fi
+}
