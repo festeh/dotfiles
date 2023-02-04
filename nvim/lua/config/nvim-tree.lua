c1ab37493b2e41bd0aab8a6ecb4d10aa8f450410
Dima Lipin <dimich3d@ya.ru>
2023-02-04 11:53:41 +0100

    smarter tree cmd

--- nvim/lua/config/nvim-tree.lua
+++ nvim/lua/config/nvim-tree.lua
@@ -1,6 +1,12 @@
 require("nvim-tree").setup()
 
+local function smart_focus()
+  if vim.api.nvim_buf_get_option(0, "buftype") ~= "" or vim.api.nvim_buf_get_name(0) == "" then
+    vim.cmd("NvimTreeToggle")
+  else
+    vim.cmd("NvimTreeFindFile")
+  end
+end
 
-vim.keymap.set({ "n", "x", "i" }, "<A-1>", "<cmd>NvimTreeFindFile<cr>")
+vim.keymap.set({ "n", "x", "i" }, "<A-1>", smart_focus)
 vim.keymap.set({ "n", "x", "i" }, "<A-2>", "<cmd>NvimTreeClose<cr>")
-
