5c1e55a740deeb83d6eba83a2147e8ba27b9b042
Dima Lipin <dimich3d@ya.ru>
2024-04-30 19:03:27 +0200

    ags + theme

--- ags/config.js
+++ ags/config.js
@@ -27,12 +27,43 @@ function Left() {
   })
 }
 
+function isOk(data) {
+  // try to deserialize the data to json
+  try {
+    const json = JSON.parse(data)
+    const res = json.DmnOn
+    if (res === undefined) {
+      return false
+    }
+    return res
+  } catch (e) {
+    return false
+  }
+}
+
+function Motivator() {
+  const goodIcon = "emblem-favorite-symbolic"
+  const badIcon = "face-sad-symbolic"
+
+  const icon = Variable(goodIcon)
+
+  const myLabel = Widget.Icon({
+    icon: icon.bind(),
+  })
+
+  setInterval(() => {
+    const res = Utils.exec("motivator")
+    icon.setValue(isOk(res) ? goodIcon : badIcon)
+  }, 3000)
+
+  return myLabel
+
+}
+
 function Center() {
   return Widget.Box({
     children: [
-      Widget.Label({
-        label: "Productivity Bar",
-      }),
+      Motivator(),
     ],
   })
 }
