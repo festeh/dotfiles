31fea767e2dc468d444ae45a6c401ec392521aea
festeh <dimich3d@ya.ru>
2024-05-06 14:42:28 +0200

    motivator fix

--- ags/config.js
+++ ags/config.js
@@ -52,8 +52,12 @@ function Motivator() {
   })
 
   setInterval(() => {
-    const res = Utils.exec("motivator")
-    icon.setValue(isOk(res) ? goodIcon : badIcon)
+    try {
+      const res = Utils.exec("motivator")
+      icon.setValue(isOk(res) ? goodIcon : badIcon)
+    } catch (e) {
+      icon.setValue(badIcon)
+    }
   }, 3000)
 
   return myLabel
@@ -216,4 +220,26 @@ App.config({
   ],
 })
 
+
+hyprland.connect("monitor-added", (_hypr, monitor) => {
+    var id = -1
+    for (var mt of hyprland.monitors) {
+        if (mt.name == monitor)
+            id = mt.id
+            break;
+    }
+    id = id == -1 ? 0 : id
+    
+    var flag = true
+    for (var wd of App.windows) {
+        if (wd.name == `bar-${id}`)
+            flag = false
+            break;
+    }
+
+    if (flag)
+        App.addWindow(Bar(id))
+})
+
 export { }
+
