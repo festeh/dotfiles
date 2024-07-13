cb480230f9b2764ec456d4f5864deeeb6b97c7b1
Dima Lipin <dimich3d@ya.ru>
2024-07-13 20:28:26 +0200

    upd

--- nvim/lua/lazy_plugins.lua
+++ nvim/lua/lazy_plugins.lua
@@ -263,6 +263,16 @@ local plugins = {
         },
       })
     end,
+    {
+      "toppair/peek.nvim",
+      event = { "VeryLazy" },
+      build = "deno task --quiet build:fast",
+      config = function()
+        require("peek").setup()
+        vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+        vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+      end,
+    },
   },
 
   -- % themes
