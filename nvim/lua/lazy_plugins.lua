70c26b00b3e07420d0477470d9a1784bfc1dfd27
festeh <dimich3d@ya.ru>
2024-07-18 09:25:40 +0200

    fix

--- nvim/lua/lazy_plugins.lua
+++ nvim/lua/lazy_plugins.lua
@@ -262,17 +262,17 @@ local plugins = {
           accept_word = "<C-j>",
         },
       })
+    end
+  },
+  {
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+      require("peek").setup()
+      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
     end,
-    {
-      "toppair/peek.nvim",
-      event = { "VeryLazy" },
-      build = "deno task --quiet build:fast",
-      config = function()
-        require("peek").setup()
-        vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-        vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-      end,
-    },
   },
 
   -- % themes
