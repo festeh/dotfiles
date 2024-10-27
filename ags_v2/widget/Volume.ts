140940fd9e1e991e2aa380e36fe6917c380d8721
Dima Lipin <dimich3d@ya.ru>
2024-10-27 20:03:18 +0100

    rm

--- ags_v2/widget/Volume.ts
+++ ags_v2/widget/Volume.ts
@@ -3,17 +3,6 @@ import { Variable, bind } from "astal"
 import GLib from "gi://GLib"
 import Wp from "gi://AstalWp"
 
-function getVolume(): number {
-  try {
-    const [success, stdout, stderr] = GLib.spawn_command_line_sync("wpctl get-volume @DEFAULT_AUDIO_SINK@")
-    const match = stdout.toString().match(/\d+\.\d+/)
-    return match ? Math.round(parseFloat(match[0]) * 100) : 0
-  } catch (error) {
-    return 0
-  }
-}
-
-const volume = Variable<number>(getVolume()).poll(1000, () => getVolume())
 
 export default function Volume() {
   const speaker = Wp.get_default()?.audio.defaultSpeaker!
@@ -27,7 +16,6 @@ export default function Volume() {
           icon: bind(muted).as(m =>
             m ? "audio-volume-muted-symbolic" : "audio-volume-high-symbolic"
           ),
-          size: 16,
         }),
         onClicked: () => {
           speaker.mute = !speaker.mute
