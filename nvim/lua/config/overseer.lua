d3e2a1232773bc9880e48e1f7f547ef6aa9227ca
Dima Lipin <dimich3d@ya.ru>
2024-11-24 09:39:57 +0100

    overseer

--- nvim/lua/config/overseer.lua
+++ nvim/lua/config/overseer.lua
@@ -1,21 +1,23 @@
 require("overseer").setup({
   strategy = {
-    "toggleterm",
-    open_on_start = false,
-    direction = "float",
-    size = 30,
-    quit_on_exit = "always"
+    "jobstart",
+    use_terminal = false,
+    -- open_on_start = false,
+    -- direction = "float",
+    -- size = 30,
+    -- quit_on_exit = "always"
   },
   component_aliases = {
     -- Most tasks are initialized with the default components
     default = {
-      { "display_duration", detail_level = 2 },
-      -- "on_output_summarize",
-      -- "on_exit_set_status",
+      { "display_duration",      detail_level = 2 },
+      "on_output_summarize",
+      "on_output_quickfix",
+      "on_exit_set_status",
       "on_complete_notify",
-      -- "on_result_diagnostics",
-      {"on_result_diagnostics", remove_on_restart = true},
-      -- { "on_complete_dispose", require_view = { "SUCCESS", "FAILURE" } },
+      "on_result_diagnostics",
+      { "on_result_diagnostics", remove_on_restart = true },
+      { "on_complete_dispose",   require_view = { "SUCCESS", "FAILURE" } },
     },
     -- Tasks from tasks.json use these components
     default_vscode = {
