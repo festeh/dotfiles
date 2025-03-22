e46e9ffaa174dc6f02815ebb3ca43239adadfd80
Dmitrii Lipin <dimich3d@ya.ru>
2025-03-22 15:59:36 +0100

    stalin

--- ags/widget/Coach.ts
+++ ags/widget/Coach.ts
@@ -3,11 +3,6 @@ import { Variable, bind } from "astal"
 import GLib from "gi://GLib"
 
 async function loadCoachUrl() {
-  // Wait for 1 second
-  await new Promise(resolve => GLib.timeout_add(GLib.PRIORITY_DEFAULT, 1000, () => {
-    resolve(true);
-    return GLib.SOURCE_REMOVE;
-  }));
   
   const coachUrl = GLib.getenv("COACH_URL")
   console.log("COACH_URL:", coachUrl)
